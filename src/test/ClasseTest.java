cad5d98a402fe14342496ae8ee318f50e1b56dfb
Josep <Josep@132.213.60.216>
2023-10-27 15:14:51 -0400

    coucou

--- src/test/ClasseTest.java
+++ src/test/ClasseTest.java
@@ -5,7 +5,8 @@ public class ClasseTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
-
+		
+		System.out.println("Hello Joseph!");
 	}
 
 }
